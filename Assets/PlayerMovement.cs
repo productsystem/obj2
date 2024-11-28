1966f91f11f91cfd2cac72b256096f0992b6eace
productsystem <prajitpradeep@gmail.com>
2024-11-29 00:53:51 +0530

    simple forward movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
+    }
+}
