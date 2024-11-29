d5f1adcae51e79e29b58874123ea05cc543f1ab9
productsystem <prajitpradeep@gmail.com>
2024-11-29 22:10:00 +0530

    fixed now. actually works properly. also added velocity flip on wall

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,15 @@ public class PlayerMovement : MonoBehaviour
     public bool canJump;
     public float colliderOffset = 0.5f;
     private Rigidbody2D rb;
+    private Vector2 rayDirection;
+
 
     // Start is called before the first frame update
     void Start()
     {
         canJump = false;
         rb = GetComponent<Rigidbody2D>();
+
     }
     // Update is called once per frame
     void FixedUpdate()
@@ -22,9 +25,20 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        rayDirection = (movementSpeed > 0) ? Vector2.right : Vector2.left;
         if(Input.GetKeyDown(KeyCode.Space) && canJump)
         {
             CheckJump();
+        }
+
+        RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, rayDirection, 0.6f);
+        for(int i = 0 ; i < hit.Length; i++)
+        {
+            if(hit[i].collider.name != "Player")
+            {
+                movementSpeed = -movementSpeed;
+                break;
+            }
         }
     }
 
@@ -48,9 +62,13 @@ public class PlayerMovement : MonoBehaviour
         {
             if(ray[i].collider.name != "Player")
             {
-                rb.MovePosition(ray[i].point);
+                Collider2D playerCollider = GetComponent<Collider2D>();
+                Vector2 colliderExtents = playerCollider.bounds.extents;
+                Vector2 adjustedPoint = ray[i].point - (Vector2)transform.up * colliderExtents.y;
+                rb.MovePosition(adjustedPoint);
                 rb.gravityScale = -rb.gravityScale;
                 transform.rotation = Quaternion.Euler(0,0,transform.eulerAngles.z + 180);
+                break;
             }
         }
     }
